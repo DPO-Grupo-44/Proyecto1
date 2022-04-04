ac8dd18badc8d54fa4127b38a5f142e052642722
SLatorre14 <s.latorre@uniandes.edu.co>
2022-04-03 19:04:07 -0500

    Creacion lista de Proyectos

---
+++ Entrega 3/proyecto3/src/proyecto3/Laboratorio.java
@@ -0,0 +1,13 @@
+package proyecto3;
+
+import java.sql.Time;
+import java.sql.Date;
+
+public class Laboratorio {
+	private ArrayList<Proyecto> listaProyectos;
+	
+	public Laboratorio() {
+		listaProyectos = new ArrayList<Proyecto>;
+		
+	}
+}
